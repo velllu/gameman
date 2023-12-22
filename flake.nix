c1c46221b9f332f6eb94cfe26bf457e01996ebb4
velllu <91963404+velllu@users.noreply.github.com>
2023-12-22 23:52:11 +0100

    Added `toolchain.toml` file

--- flake.nix
+++ flake.nix
@@ -1,31 +1,34 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            cargo rustc rustfmt pre-commit rust-analyzer rustPackages.clippy
-            libxkbcommon libGL wayland xorg.libXcursor xorg.libXrandr xorg.libXi xorg.libX11
-          ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        toolchain
 
-          LD_LIBRARY_PATH = builtins.concatStringsSep ":" [
-            "${pkgs.xorg.libX11}/lib"
-            "${pkgs.xorg.libXi}/lib"
-            "${pkgs.libGL}/lib"
-          ];
+        pkgs.libxkbcommon pkgs.libGL pkgs.wayland pkgs.xorg.libXcursor
+        pkgs.xorg.libXrandr pkgs.xorg.libXi pkgs.xorg.libX11
+      ];
 
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      });
+      LD_LIBRARY_PATH = builtins.concatStringsSep ":" [
+        "${pkgs.xorg.libX11}/lib"
+        "${pkgs.xorg.libXi}/lib"
+        "${pkgs.libGL}/lib"
+      ];
+    };
+  };
 }
